ab1c48be937ff500f66993783655c89da5bfaa6f
belkaxoy <belkaxoy@yandex.ru>
2020-11-29 18:00:28 +0300

    added text-type component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as $ from 'jquery'
 import Post from '@models/Post'
+import './styles/vars.sass'
 import './styles/styles.css'
-import './styles/scss.scss'
 import json from './assets/json'
 import logo from '@/assets/img/logo.svg'
 import './babel'
